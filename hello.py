44f7a52507e7ce9ef7b9e0a33dd36465d472c11d
Vishesh <112301037@smail.iitpkd.ac.in>
2024-04-09 04:52:46 +0530

    finall stages of css in base and todo. added images

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,jsonify,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for
 from datetime import datetime
 from googletrans import Translator,LANGUAGES
 import os,csv
