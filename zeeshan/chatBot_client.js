abc60143edea4e5e858d757ed87d3bc3a885e89e
Zeeshan1903 <156703447+Zeeshan1903@users.noreply.github.com>
2024-04-29 20:22:43 +0530

    Update chatBot_client.js

--- zeeshan/chatBot_client.js
+++ zeeshan/chatBot_client.js
@@ -3,10 +3,31 @@ const socket = io();
 const form = document.getElementById('form');
 const button = document.getElementById('submit');
 const activeUsertag = document.getElementById('activeUser')
-button.addEventListener('click', (e) => {
-    e.preventDefault();
 
-});
+
+// const getOtpBtn = document.getElementById('getOtpBtn');
+// getOtpBtn.addEventListener('click', async () => {
+//     const email = document.getElementById('email').value;
+//     const response = await fetch('/send-otp', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ email: email })
+//     });
+//     if (response.ok) {
+//         console.log('OTP sent successfully');
+//         // After OTP is sent successfully, show the OTP input field
+//         document.getElementById('otp').style.display = 'block';
+//     } else {
+//         console.error('Failed to send OTP');
+//     }
+// });
+
+
+// const otpCheck = document.getElementById('otp').value
+
+
 
 // Fetch user details and display them here we are just asking for the user detail to make them display also 
 async function getUserDetail() {
